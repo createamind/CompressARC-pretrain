a2ca7511fdf232fd1cb1d46ac5a2a169b2c3db65
zdx <179363811@qq.com>
2025-08-05 07:41:06 +0800

    v8 two_stage_train.py lr cycle

--- train_rule_guided.py
+++ train_rule_guided.py
@@ -150,7 +150,14 @@ def train_rule_guided_from_pretrained(
 
     # 定义优化器和学习率调度器
     optimizer = optim.AdamW(trainable_params, lr=learning_rate, weight_decay=1e-5)
-    scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=epochs, eta_min=1e-6)
+    # scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=epochs, eta_min=1e-6)
+    scheduler = torch.optim.lr_scheduler.CyclicLR(
+    optimizer,
+    base_lr=0.0007,
+    max_lr=0.002,
+    step_size_up=10,
+    cycle_momentum=False
+)
 
     # 检查是否有需要恢复的训练断点
     start_epoch = 0
